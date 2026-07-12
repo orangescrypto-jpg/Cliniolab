75bdec4cb0d8fff35a07769176e2aed5e87cf7b1
orangescrypto-jpg <orangescrypto@gmail.com>
2026-07-12 17:29:12 +0100

    Update PwaSetup.tsx

--- src/components/layout/PwaSetup.tsx
+++ src/components/layout/PwaSetup.tsx
@@ -8,11 +8,15 @@ interface BeforeInstallPromptEvent extends Event {
   userChoice: Promise<{ outcome: 'accepted' | 'dismissed' }>;
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
+  const bytes = new Uint8Array(new ArrayBuffer(rawData.length));
+  for (let i = 0; i < rawData.length; i++) {
+    bytes[i] = rawData.charCodeAt(i);
+  }
+  return bytes;
 }
 
 /**
